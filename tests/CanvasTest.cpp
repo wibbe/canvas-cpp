0276e8eb1cc01283108a1bb16b3e4de57a73d4fe
Daniel Wiberg <d.a.wiberg@gmail.com>
2010-08-10 22:57:59 +0200

    Updated the javascript tests

--- tests/CanvasTest.cpp
+++ tests/CanvasTest.cpp
@@ -36,7 +36,7 @@ namespace tut
    {
       set_test_name("Javascript Test 1");
       
-      canvas->loadCode("(function() { })();");
+      canvas->loadCode("");
    }
    
    template <>
@@ -45,6 +45,45 @@ namespace tut
    {
       set_test_name("Javascript Test 2: window.log()");
       
-      //canvas->loadCode("");
+      canvas->loadCode("window.log(\"Hello, World!\")");
+      ensure(canvas->lastLogEntry() == "Hello, World!");
+   }
+   
+   template <>
+   template <>
+   void object::test<4>()
+   {
+      set_test_name("Javascript Test 3: window.setInterval()");
+      
+      canvas->loadCode("window.setInterval(function() { window.log(\"Hello, World!\"); });");
+      ensure(canvas->lastLogEntry() == "");
+      
+      canvas->paint(0);
+      canvas->paint(0);
+      
+      // After we have painter two times, we should have two "Hello, World!" messages in the history
+      ensure(canvas->lastLogEntry() == "Hello, World!");
+      ensure(canvas->lastLogEntry() == "Hello, World!");
+      ensure(canvas->lastLogEntry() == "");
+   }
+   
+   template <>
+   template <>
+   void object::test<5>()
+   {
+      set_test_name("Javascript Test 4: window.getContext('')");
+      
+      canvas->loadCode("window.log(window.getContext('') == undefined ? 'True' : 'False')");
+      ensure(canvas->lastLogEntry() == "True");
+   }
+   
+   template <>
+   template <>
+   void object::test<6>()
+   {
+      set_test_name("Javascript Test 5: window.getContext('2d')");
+      
+      canvas->loadCode("window.log(window.getContext('2d') != undefined ? 'True' : 'False')");
+      ensure(canvas->lastLogEntry() == "True");
    }
 }
