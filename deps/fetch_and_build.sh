bd3e4ccba95f51b76992b80da600849fb01290a1
Daniel Wiberg <daniel.wiberg@oryx.se>
2010-08-11 11:02:41 +0200

    Updated build script to work under linux.

--- deps/fetch_and_build.sh
+++ deps/fetch_and_build.sh
@@ -1,12 +1,19 @@
 #! /bin/sh
 
 cd `dirname $0`
+platform=`uname`
 
 echo "Fetching Skia..."
 svn checkout http://skia.googlecode.com/svn/trunk/ skia
 
 cd skia
-make SKIA_BUILD_FOR=mac SKIA_DEBUG=true
+if [$platform -eq 'darwin' ] ; then
+  echo "Building Skia for mac..."
+  make SKIA_BUILD_FOR=mac SKIA_DEBUG=true
+else
+  echo "Building Skia for linux..."
+  make SKIA_DEBUG=true
+fi
 
 cd ..
 
@@ -16,3 +23,7 @@ svn checkout http://v8.googlecode.com/svn/trunk/ v8
 cd v8
 echo "Building V8..."
 scons mode=debug library=static snapshot=off -j4
+
+if [$platform -eq 'Linux'] ; then
+  mv libv8_g.a libv8.a
+fi
