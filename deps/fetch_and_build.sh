14521b336c232eb02c53c2ad335721d854078ee3
Daniel <wibbe@blueeye.se>
2010-10-26 00:27:22 -0700

    Added revision number to both skia and v8

--- deps/fetch_and_build.sh
+++ deps/fetch_and_build.sh
@@ -4,10 +4,10 @@ cd `dirname $0`
 platform=`uname`
 
 echo "Fetching Skia..."
-svn checkout http://skia.googlecode.com/svn/trunk/ skia
+svn checkout -r 614 http://skia.googlecode.com/svn/trunk/ skia
 
 cd skia
-if [$platform -eq 'darwin' ] ; then
+if [ $platform -eq 'darwin' ] ; then
   echo "Building Skia for mac..."
   make SKIA_BUILD_FOR=mac SKIA_DEBUG=true
 else
@@ -18,12 +18,12 @@ fi
 cd ..
 
 echo "Fetching V8..."
-svn checkout http://v8.googlecode.com/svn/trunk/ v8
+svn checkout -r 5700 http://v8.googlecode.com/svn/trunk/ v8
 
 cd v8
 echo "Building V8..."
 scons mode=debug library=static snapshot=off -j4
 
-if [$platform -eq 'Linux'] ; then
+if [ $platform -eq 'Linux' ] ; then
   mv libv8_g.a libv8.a
 fi
